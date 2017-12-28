354bd07274028d9bb4fd1615a736edaf56334dcd
Joe Heck <heckj@mac.com>
2017-12-28 11:15:02 -0800

    adding initial deployment, username & password config

--- celery_conf.py
+++ celery_conf.py
@@ -3,11 +3,17 @@
 import os
 from celery import Celery
 
+# set username and password for broker, with overrides from environment variables
+rabbitmq_user = os.environ.get('RABBITMQ_USERNAME','user')
+rabbitmq_password = os.environ.get('RABBITMQ_USERNAME','bitnami')
+rabbitmq_vhost = os.environ.get('RABBITMQ_VHOST','/')
+
 # Get Kubernetes-provided address of the broker service
-broker_service_host = os.environ.get('MESSAGE_QUEUE_SERVICE_HOST')
+broker_service_host = os.environ.get('MESSAGE_QUEUE_SERVICE_HOST','message-queue')
 # could also use DNS name: 'message-queue'|'message-queue.default.svc.cluster.local' for our default
 
-app = Celery('tasks', broker='amqp://user@%s//' % broker_service_host, backend='amqp')
+broker_url = 'amqp://%s:%s@%s/%s' % rabbitmq_user, rabbitmq_password, broker_service_host
+app = Celery('tasks', broker=broker_url, backend='amqp')
 
 @app.task
 def add(x, y):
