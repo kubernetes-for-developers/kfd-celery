766d3458e9231a37e1598bcd8799f15a3d56dec2
Joe Heck <heckj@mac.com>
2017-12-28 13:31:13 -0800

    fixing broker_url creation

--- celery_conf.py
+++ celery_conf.py
@@ -12,7 +12,8 @@ rabbitmq_vhost = os.environ.get('RABBITMQ_VHOST','/')
 broker_service_host = os.environ.get('MESSAGE_QUEUE_SERVICE_HOST','message-queue')
 # could also use DNS name: 'message-queue'|'message-queue.default.svc.cluster.local' for our default
 
-broker_url = 'amqp://%s:%s@%s/%s' % rabbitmq_user, rabbitmq_password, broker_service_host
+broker_url = 'amqp://%s:%s@%s/%s' % (rabbitmq_user,
+        rabbitmq_password, broker_service_host, rabbitmq_vhost)
 app = Celery('tasks', broker=broker_url, backend='amqp')
 
 @app.task
