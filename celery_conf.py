ab2b4584488c5822fb38e2221f557d900265df9a
Joe Heck <heckj@mac.com>
2017-12-29 16:22:50 -0800

    fix typo

--- celery_conf.py
+++ celery_conf.py
@@ -12,7 +12,7 @@ rabbitmq_vhost = os.environ.get('RABBITMQ_VHOST','/')
 
 # Get Kubernetes-provided address of the broker service
 broker_service_host = os.environ.get('MESSAGE_QUEUE_SERVICE_HOST','message-queue')
-# could also use DNS name: 'message-queue'|'message-queue.default.svc.cluster.local' 
+# could also use DNS name: 'message-queue'|'message-queue.default.svc.cluster.local'
 # for our default
 
 broker_url = 'amqp://%s:%s@%s/%s' % (rabbitmq_user,
@@ -24,7 +24,7 @@ def add(x, y):
     logger.info('Adding {0} + {1}'.format(x, y))
     return x + y
 
-@app.task(bind=true)
+@app.task(bind=True)
 def dump_context(self, x, y):
     logger.info('Executing task id {0.id}, args: {0.args!r} kwargs: {0.kwargs!r}'.format(
             self.request))
